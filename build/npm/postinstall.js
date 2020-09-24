145082be69e80dad062f520c198bf1f9ee1b4ced
JiangCX <ldh758945@outlook.com>
2020-09-24 23:30:31 +0800

    yarn install  以及install前后的依赖注入，补充注释文件 安装插件以及安装watch

--- build/npm/postinstall.js
+++ build/npm/postinstall.js
@@ -14,43 +14,58 @@ const yarn = process.platform === 'win32' ? 'yarn.cmd' : 'yarn';
  */
 function yarnInstall(location, opts) {
 	opts = opts || { env: process.env };
+	// 子进程的当前工作目录 指向目标文件目录
 	opts.cwd = location;
+	// 子进程的 stdio 配置
 	opts.stdio = 'inherit';
-
+	// raw: {"remain":[],"cooked":["run","postinstall"],"original":["run","postinstall"]}
 	const raw = process.env['npm_config_argv'] || '{}';
 	const argv = JSON.parse(raw);
+	// "original":["run","postinstall"]
 	const original = argv.original || [];
+	// 需要执行的指令list
 	const args = original.filter(arg => arg === '--ignore-optional' || arg === '--frozen-lockfile');
 
 	console.log(`Installing dependencies in ${location}...`);
 	console.log(`$ yarn ${args.join(' ')}`);
+	// 在opts.cwd目录下执行命令
 	const result = cp.spawnSync(yarn, args, opts);
 
 	if (result.error || result.status !== 0) {
 		process.exit(1);
 	}
 }
-
+// 依赖注入，在extensions目录下执行yarn命令: 安装指定的依赖项
 yarnInstall('extensions'); // node modules shared by all extensions
-
+// !(32位的win系统 && (处理器架构是 arm64 ||  process.env['npm_config_arch'] === 'arm64'))
 if (!(process.platform === 'win32' && (process.arch === 'arm64' || process.env['npm_config_arch'] === 'arm64'))) {
 	yarnInstall('remote'); // node modules used by vscode server
 	yarnInstall('remote/web'); // node modules used by vscode web
 }
 
+// 同步读取extensions文件夹下的所有文件
 const allExtensionFolders = fs.readdirSync('extensions');
+// extensions输出: ['bat', 'clojure']
 const extensions = allExtensionFolders.filter(e => {
 	try {
+		// path.join('/目录1', '目录2', '目录3/目录4', '目录5', '..');
+		// 返回: '/目录1/目录2/目录3/目录4'
+		// 返回: extensions/bat/package.json
 		let packageJSON = JSON.parse(fs.readFileSync(path.join('extensions', e, 'package.json')).toString());
+		// 如果package.json存在 && ( dependencies字段指定了项目运行所依赖的模块 || devDependencies指定项目开发所需要的模块 )
 		return packageJSON && (packageJSON.dependencies || packageJSON.devDependencies);
 	} catch (e) {
 		return false;
 	}
 });
-
+// 在`extensions/${extension}`目录下安装依赖： 执行 yarn命令
 extensions.forEach(extension => yarnInstall(`extensions/${extension}`));
 
+/**
+ * 安装构建依赖项
+ */
 function yarnInstallBuildDependencies() {
+	// 确保我们的系统安装了watch
 	// make sure we install the deps of build/lib/watch for the system installed
 	// node, since that is the driver of gulp
 	const watchPath = path.join(path.dirname(__dirname), 'lib', 'watch');
@@ -63,7 +78,7 @@ function yarnInstallBuildDependencies() {
 	const yarnrc = `disturl "${disturl}"
 target "${target}"
 runtime "${runtime}"`;
-
+	// TODO？应该是将错误日志记录到了yarnrc中
 	fs.writeFileSync(yarnrcPath, yarnrc, 'utf8');
 	yarnInstall(watchPath);
 }
--- build/npm/preinstall.js
+++ build/npm/preinstall.js
@@ -7,27 +7,38 @@ let err = false;
 
 const majorNodeVersion = parseInt(/^(\d+)\./.exec(process.versions.node)[1]);
 
+// 如果node的版本大于13或者小于10 退出代码执行 err掷为true
 if (majorNodeVersion < 10 || majorNodeVersion >= 13) {
 	console.error('\033[1;31m*** Please use node >=10 and <=12.\033[0;0m');
 	err = true;
 }
 
+// 子进程
 const cp = require('child_process');
+// 执行命令 查询yarn版本 主版本号/次版本号/修订号
 const yarnVersion = cp.execSync('yarn -v', { encoding: 'utf8' }).trim();
-const parsedYarnVersion = /^(\d+)\.(\d+)\./.exec(yarnVersion);
+// 对输出内容进行格式化
+const parsedYarnVersion = /^(\d+)\.(\d+)\.(\d+)/.exec(yarnVersion);
+// 主版本号
 const majorYarnVersion = parseInt(parsedYarnVersion[1]);
+// 次版本号
 const minorYarnVersion = parseInt(parsedYarnVersion[2]);
+// 修订号
+const patchYarnVersion = parseInt(parsedYarnVersion[3]);
 
+// 限制yarn的版本
 if (majorYarnVersion < 1 || minorYarnVersion < 10) {
 	console.error('\033[1;31m*** Please use yarn >=1.10.1.\033[0;0m');
 	err = true;
 }
 
+// 限制使用yarn安装依赖
 if (!/yarn[\w-.]*\.js$|yarnpkg$/.test(process.env['npm_execpath'])) {
 	console.error('\033[1;31m*** Please use yarn to install dependencies.\033[0;0m');
 	err = true;
 }
 
+// 如果存在错误 那么终止线程
 if (err) {
 	console.error('');
 	process.exit(1);
